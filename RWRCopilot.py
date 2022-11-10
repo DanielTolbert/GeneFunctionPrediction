c7294c7fdee937deb652cb765b1284c6940832ff
thriloks <thrlioks@vt.edu>
2022-11-10 15:58:17 -0500

    Working on RWR testing

--- RWRCopilot.py
+++ RWRCopilot.py
@@ -11,10 +11,14 @@ class RWRRunner:
         self.G = nx.convert_node_labels_to_integers(self.G, first_label=0, ordering='default', label_attribute=None)
         N = self.G.number_of_nodes()
         Nodes = self.G.nodes()
-        NodeDict = dict(zip(Nodes, range(N)))
-        Source = NodeDict[self.Source]
+        # NodeDict = dict(zip(Nodes, range(N)))
+        # for node, n in NodeDict.items():
+            # print("{}   ({})".format(node, n))
+        # Source = NodeDict[self.Source]
+        Source = self.Source
         RestartProb = 0.15
         P = nx.to_numpy_matrix(self.G)
+        # print(P.sum(axis=0))
         P = P / P.sum(axis=0)
         P = (1 - RestartProb) * P + RestartProb * (1 / N)
         A = P.copy()
@@ -23,10 +27,6 @@ class RWRRunner:
             P = P + A
         P = P / P.sum(axis=0)
         return P[:, Source].T.tolist()[0]
-
-    def RandomWalkTest(self) -> dict:
-        RWRCopilot = RWRRunner(self.G, self.Source)
-        return RWRCopilot.RandomWalkWithRestarts()
 
 
 OldG = nx.karate_club_graph()
@@ -38,12 +38,13 @@ OldG = nx.karate_club_graph()
 #     edges = file.readlines()
 #     for edge in edges:
 #         OldG.add_edge(edge.split(",")[0], edge.split(",")[1])
-print(OldG.number_of_nodes())
-print(OldG.number_of_edges())
+# print(OldG.number_of_nodes())
+# print(OldG.number_of_edges())
 
-print(type(OldG.nodes()))
+# for node in OldG.nodes():
+    # print(node)
 
-runner = RWRRunner(OldG)
-print(runner.RandomWalkTest())
+runner = RWRRunner(OldG, {34, 22, 32})
+print(runner.RandomWalkWithRestarts())
 
 
