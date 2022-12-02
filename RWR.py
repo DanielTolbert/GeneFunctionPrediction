550299a4b225a1204d882237032ce821630730b6
thriloks <thrlioks@vt.edu>
2022-12-01 19:11:02 -0500

    RWR Fully working!

--- RWR.py
+++ RWR.py
@@ -1,28 +1,43 @@
 import networkx as nx
 import numpy as np
+from parser_module import *
+import time
+# To run, python .\RWR.py > your_output_file_name.txt
 
-G = nx.karate_club_graph()
+t = time.time()
+data = Annotation_Parser('go.obo', 'goa_human.gaf', 'human_interactome_weighted.txt', threshold=1)
+G = data.interactome
 
-adj = nx.adjacency_matrix(G, None, None, None)
+# The first run will take about 14 minutes, but once the run is complete, will make subsequent runs faster.
+# Uncomment lines 13 to 29 for the first run, and comment out line 33 for the first run.
+# adj = nx.adjacency_matrix(G, None, None, None)
 
-Sources = [1, 12, 17, 33]
+# s = np.zeros(adj.shape[0])
 
-s = np.zeros(adj.shape[0])
+# diag = np.diag(np.sum(adj, axis=1).A1)
 
-for Source in Sources:
-    s[Source] = 1 / len(Sources)
+# inverseDiag = np.linalg.pinv(diag)
 
-diag = np.diag(np.sum(adj, axis=1).A1)
+# prod = inverseDiag * adj
 
-inverseDiag = np.linalg.inv(diag)
+# prodTranspose = prod.transpose()
 
-prod = inverseDiag * adj
+# identityMatrix = np.identity(adj.shape[0])
 
-prodTranspose = prod.transpose()
+# vectorStationaryProbabilities = np.linalg.inv(identityMatrix - ((1 - 0.5) * prodTranspose))
 
-identityMatrix = np.identity(adj.shape[0])
+# np.save('vectorStationaryProbabilities.npy', vectorStationaryProbabilities)
 
-vectorStationaryProbabilities = np.linalg.inv(identityMatrix - ((1 - 0.5) * prodTranspose))
+# After the first run, re comment lines 13 to 29, and uncomment line 33
+
+vectorStationaryProbabilities = np.load('vectorStationaryProbabilities.npy')
+# For each go term, change the value below, I suggest using a different output file each time.
+pos, neg = data.examples('GO:0048523')
+# print(pos) # In order to randomly set some positive examples to 0, set newVar equal to the new value of pos with some positive values set to 0.
+# newVar = 
+# Replace pos in line 38 with newVar
+s = pos / np.sum(pos)
+
 
 a = np.array(vectorStationaryProbabilities)
 
@@ -30,11 +45,19 @@ b = np.array(s)
 
 vectorStationaryProbabilities = np.dot(a, b) * .5
 
-print(vectorStationaryProbabilities)
-
 dictVectorStationaryProbabilities = dict(zip(G.nodes(), vectorStationaryProbabilities))
 
 value_key_pairs = ((value, key) for (key,value) in dictVectorStationaryProbabilities.items())
 sorted_value_key_pairs = sorted(value_key_pairs, reverse=True)
-
-print(sorted_value_key_pairs)
+t2 = time.time()
+print("Run took seconds: ", t2 - t)
+print("Below is the positive examples: ")
+nodeDict = dict(zip(data.genes, range(len(G.nodes()))))
+for i in range(len(sorted_value_key_pairs)):
+    if pos[nodeDict.get(sorted_value_key_pairs[i][1])] == 1:
+        print("Node: ", sorted_value_key_pairs[i][1], " Score: ", sorted_value_key_pairs[i][0])
+    elif neg[nodeDict.get(sorted_value_key_pairs[i][1])] == 1:
+        print("Node: ", sorted_value_key_pairs[i][1], " Score: ", sorted_value_key_pairs[i][0], " False Positive")
+    else:
+        print("Node: ", sorted_value_key_pairs[i][1], " Score: ", sorted_value_key_pairs[i][0], " Non Example")
+print(np.sum(vectorStationaryProbabilities))
